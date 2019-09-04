6f9a9f6e2d96bd2c2edb350ab632bb053d2ebf41
JAtherton <atherton374@btinternet.com>
2019-09-04 23:21:31 +0100

    arrays and hashes

---
+++ exercise_a.rb
@@ -0,0 +1,46 @@
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+# stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+# 1. Add `"Edinburgh Waverley"` to the end of the array
+
+  #stops.push("Edinburgh Waverley")
+
+# 2. Add `"Glasgow Queen St"` to the start of the array
+
+  #stops.unshift("Glasgow Queen St")
+
+# 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and
+# `"Linlithgow"`)
+
+ #stops.insert(3, "Polmont")
+
+# 4. Work out the index position of `"Linlithgow"`
+
+  #stops.index("Linlithgow")
+
+# 5. Remove `"Livingston"` from the array using its name
+
+  #stops.delete("Livingston")
+  #puts stops
+
+
+# 6. Delete `"Cumbernauld"` from the array by index
+  #stops.delete_at(1)
+  #puts stops
+
+# 7. How many stops there are in the array?
+
+ #puts stops.count
+
+# 8. How many ways can we return `"Falkirk High"` from the array?
+
+ #puts stops.count("Falkirk High")
+
+# 9. Reverse the positions of the stops in the array
+
+  #puts stops.reverse
+
+# 10. Print out all the stops using a for loop
+
+   for x in stops
+     print x
+   end   
